5299161ae6183348a3a94252e46d845028f854f0
Andojo Mack <andojomack@gmail.com>
2022-03-09 01:00:35 -0300

    added test data

---
+++ src/app/Medewerker.ts
@@ -0,0 +1,12 @@
+export interface Medewerker {
+    id?: number;
+    voornaam: string;
+    achternaam: string;
+    geslacht: string;
+    email: string;
+    telefoon: number;
+    straatnaam: string;
+    huisnummer: string;
+    district: string;
+    active: boolean;
+}
