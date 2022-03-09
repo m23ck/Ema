821f50e71c100e9804fcd8813298dcd0b6b0bc3e
Andojo Mack <andojomack@gmail.com>
2022-03-09 01:01:11 -0300

    added the name ema and an add function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ema';
+  title: string = 'ema';
+
+  
+
+  toggleAddMedewerker() {
+    console.log('toggle')
+  }
 }
