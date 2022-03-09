9dd7c448645cc254a86bd986f8a0a86788d5c3ca
Andojo Mack <andojomack@gmail.com>
2022-03-09 01:00:51 -0300

    auto added modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { MedewerkersComponent } from './components/medewerkers/medewerkers.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './views/home/home.component';
+import { MedewerkersbeheerComponent } from './views/medewerkersbeheer/medewerkersbeheer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ButtonComponent,
+    MedewerkersComponent,
+    HomeComponent,
+    MedewerkersbeheerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
