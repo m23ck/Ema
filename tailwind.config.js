f71038c37bb4a2c63c4ad7878c4e1e1298ccbaf3
Andojo Mack <andojomack@gmail.com>
2022-03-09 01:02:44 -0300

    added custom colors and fonts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'blue': '#0D67A8',
+      'green': '#2EA02F',
+      'orange': '#EB7E1A',
+      'red': '#FF0000',
+      'white': '#FFFFFF',
+      'gray': '#D9D9D9',
+      'light-gray': "#FBFBFB",
+      
+    },
+    fontFamily: {
+      'body': ['"Open Sans"'],
+      'display': ['"Quicksand"'],
+    },
     extend: {},
   },
   plugins: [],
