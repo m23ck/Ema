4eebffe2c219a7e3fd2f668eeec196349b099134
Andojo Mack <andojomack@gmail.com>
2022-03-12 11:24:39 -0300

    created form and input components for add/edit med

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  @Input()
+  name!: string;
+
+  @Input()
+  type!: string;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
