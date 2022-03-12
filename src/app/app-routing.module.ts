ddbe92117cdd5cd0cd48d9b2fd0ff1990f71141a
Andojo Mack <andojomack@gmail.com>
2022-03-12 11:23:19 -0300

    added routes for crud medewerkers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,22 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
 import { MedewerkersbeheerComponent } from './views/medewerkersbeheer/medewerkersbeheer.component';
+import { AddMedewerkerComponent } from './views/add-medewerker/add-medewerker.component';
+import { EditMedewerkerComponent } from './views/edit-medewerker/edit-medewerker.component';
+import { ViewMedewerkerComponent } from './views/view-medewerker/view-medewerker.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'medewerkersbeheer', component: MedewerkersbeheerComponent},
+  {
+    path: 'medewerkers',
+    children: [
+      {path: '', component: MedewerkersbeheerComponent},
+      {path: 'add-medewerker', component: AddMedewerkerComponent},
+      {path: 'edit-medewerker/:id', component: EditMedewerkerComponent},
+      {path: 'change-medewerker-status/:id', component: MedewerkersbeheerComponent},
+      {path: 'view-medewerker/:id', component: ViewMedewerkerComponent},
+    ]
+  }
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
