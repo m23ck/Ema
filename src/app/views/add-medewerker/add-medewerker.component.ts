9ea18d58bb1f54e2052d5f0ef788b4fe19c35fba
Andojo Mack <andojomack@gmail.com>
2022-03-13 13:28:44 -0300

    created edit functionality | works

--- src/app/views/add-medewerker/add-medewerker.component.ts
+++ src/app/views/add-medewerker/add-medewerker.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MedewerkersService } from 'src/app/components/medewerkers/medewerkers.service';
+// const { toast, snackbar } = require('tailwind-toast')
+
 
 @Component({
   selector: 'app-add-medewerker',
@@ -6,14 +10,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-medewerker.component.css']
 })
 export class AddMedewerkerComponent implements OnInit {
-
-  constructor() { }
+  addMedewerkerForm: FormGroup = new FormGroup({});
+  constructor(private formBuilder: FormBuilder, private medewerkersService: MedewerkersService) { }
 
   ngOnInit(): void {
+    this.addMedewerkerForm = this.formBuilder.group({
+      'voornaam': new FormControl('', [Validators.required]),
+      'achternaam': new FormControl('', [Validators.required]),
+      'geslacht': new FormControl('', [Validators.required]),
+      'email': new FormControl('', [Validators.required]),
+      'telefoon': new FormControl('', [Validators.required]),
+      'straatnaam': new FormControl('', [Validators.required]),
+      'huisnummer': new FormControl('', [Validators.required]),
+      'district': new FormControl('', [Validators.required]),
+      'isActive': new FormControl(true, [Validators.required])
+    })
   }
 
-  addMedewerker(){
-    
+  createMedewerker(){
+    this.medewerkersService.addMedewerker(this.addMedewerkerForm.value).subscribe(data => {
+      console.log("Medewerker Created")
+      // toast()
+      // .warning('Medewerker Gecreeerd en geactiveerd!')
+      // .with({
+      //   shape: 'pill',
+      //   duration: 2500,
+      //   speed: 1000,
+      //   positionX: 'end',
+      //   positionY: 'top',
+      //   color: 'bg-blue',
+      //   fontColor: 'white',
+      //   fontTone: 200
+      // }).show() //display with all parameters
+    },
+    err => {
+      console.log(err)
+      // toast()
+      // .warning('An error has occurred!')
+      // .with({
+      //   shape: 'pill',
+      //   duration: 2500,
+      //   speed: 1000,
+      //   positionX: 'end',
+      //   positionY: 'top',
+      //   color: 'bg-red',
+      //   fontColor: 'white',
+      //   fontTone: 200
+      // }).show() //display with all parameters
+    })
   }
 
 }
--- src/app/views/edit-medewerker/edit-medewerker.component.ts
+++ src/app/views/edit-medewerker/edit-medewerker.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { MedewerkersService } from 'src/app/components/medewerkers/medewerkers.service';
+import { Medewerker } from 'src/app/Medewerker';
 
 @Component({
   selector: 'app-edit-medewerker',
@@ -8,18 +10,55 @@ import { MedewerkersService } from 'src/app/components/medewerkers/medewerkers.s
   styleUrls: ['./edit-medewerker.component.css']
 })
 export class EditMedewerkerComponent implements OnInit {
-  medewerkerId: string = '';
-  constructor(private medewerkersService: MedewerkersService, private activatedRoute: ActivatedRoute) { }
+  medewerkerId: number = 0;
+  medewerker: Medewerker | undefined;
+  editMedewerkerForm: FormGroup = new FormGroup({});
+  dataLoaded: boolean = false;
+
+
+  constructor(private medewerkersService: MedewerkersService, private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder) { }
 
 
   ngOnInit(): void {
+    this.dataLoaded = false;
     const medewerkerId = this.activatedRoute.params.subscribe(data => {
       this.medewerkerId = data['id'];
     })
+    // get medewerker details
+    if(this.medewerkerId !== 0){
+      this.medewerkersService.viewMedewerker(this.medewerkerId)
+      .toPromise()
+      .then(data => {
+        this.medewerker = data;
+        console.log(this.medewerker)
+        // build the edit form
+      this.editMedewerkerForm = this.formBuilder.group({
+        'voornaam': new FormControl(this.medewerker?.voornaam),
+        'achternaam': new FormControl(this.medewerker?.achternaam),
+        'geslacht': new FormControl(this.medewerker?.geslacht),
+        'email': new FormControl(this.medewerker?.email),
+        'telefoon': new FormControl(this.medewerker?.telefoon),
+        'straatnaam': new FormControl(this.medewerker?.straatnaam),
+        'huisnummer': new FormControl(this.medewerker?.huisnummer),
+        'district': new FormControl(this.medewerker?.district),
+        'isActive': new FormControl(this.medewerker?.isActive)
+      })
+      this.dataLoaded = true;
+    })
+.catch(err => {
+      console.log(err)
+    })
+      
+    }
+  }
 
-    this.medewerkersService.viewMedewerker(this.medewerkerId).subscribe(data => {
-      console.log(data);
-    });
+  updateMedewerker(){
+    this.medewerkersService.updateMedewerker(this.medewerkerId, this.editMedewerkerForm.value).subscribe(data => {
+      console.log("medewerker geupdate")
+    },err => {
+        console.log(err)
+      }
+  )
   }
 
 }
