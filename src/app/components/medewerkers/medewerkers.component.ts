f6961841a5f19ac9a5b230f50b47ca7f03e5bb57
Andojo Mack <andojomack@gmail.com>
2022-03-13 22:44:18 -0300

    added table controls and styled

--- src/app/components/medewerkers/medewerkers.component.ts
+++ src/app/components/medewerkers/medewerkers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MedewerkersService } from './medewerkers.service';
 import { Medewerker } from 'src/app/Medewerker';
 import {TableModule} from 'primeng/table';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-medewerkers',
@@ -11,11 +12,30 @@ import {TableModule} from 'primeng/table';
 export class MedewerkersComponent implements OnInit {
 
   public medewerkers: Medewerker[] = [];
+  medewerkerId: number = 0;
+  medewerker: Medewerker | undefined;
+  cols!: any[];
 
-  constructor(private medewerkersService: MedewerkersService) { }
+  constructor(private medewerkersService: MedewerkersService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getMedewerkers();
+    const medewerkerId = this.activatedRoute.params.subscribe(data => {
+      this.medewerkerId = data['id'];
+      this.medewerker = data['medewerker'];
+    })
+
+    this.cols = [
+      { field: 'voornaam', header: 'Voornaam' },
+      { field: 'achternaam', header: 'Achternaam' },
+      { field: 'geslacht', header: 'Geslacht' },
+      { field: 'email', header: 'Email' },
+      { field: 'telefoon', header: 'Telefoon' },
+      { field: 'straatnaam', header: 'Straatnaam' },
+      { field: 'huisnummer', header: '#' },
+      { field: 'district', header: 'District' },
+      { field: 'actief', header: 'Actief' }
+  ];
   }
 
   getMedewerkers() {
@@ -23,4 +43,13 @@ export class MedewerkersComponent implements OnInit {
       (response: Medewerker[]) => this.medewerkers = response
     )
   }
+
+  // changeMedewerkerStatus(){
+  //   this.medewerkersService.updateMedewerker(this.medewerkerId, this.medewerker ).subscribe(data => {
+  //     console.log("medewerker geupdate")
+  //   },err => {
+  //       console.log(err)
+  //     }
+  // )
+  // }
 }
