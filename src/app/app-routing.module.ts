70638babb0e4456dbf3630ab3ff7e11b935bac52
Andojo Mack <andojomack@gmail.com>
2022-03-14 02:52:17 -0300

    added change-medewerker-status route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { MedewerkersbeheerComponent } from './views/medewerkersbeheer/medewerker
 import { AddMedewerkerComponent } from './views/add-medewerker/add-medewerker.component';
 import { EditMedewerkerComponent } from './views/edit-medewerker/edit-medewerker.component';
 import { ViewMedewerkerComponent } from './views/view-medewerker/view-medewerker.component';
+import { ChangeMedewerkerStatusComponent } from './components/change-medewerker-status/change-medewerker-status.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -16,7 +17,7 @@ const routes: Routes = [
       {path: '', component: MedewerkersbeheerComponent},
       {path: 'add-medewerker', component: AddMedewerkerComponent},
       {path: 'edit-medewerker/:id', component: EditMedewerkerComponent},
-      {path: 'change-medewerker-status/:id', component: MedewerkersbeheerComponent},
+      {path: 'change-medewerker-status/:id', component: ChangeMedewerkerStatusComponent},
       {path: 'view-medewerker/:id', component: ViewMedewerkerComponent},
     ]
   }
