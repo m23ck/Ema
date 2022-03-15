c1669feb44a0684d00ec93ce4f08cf74ce7f9ef9
Andojo Mack <andojomack@gmail.com>
2022-03-14 23:32:38 -0300

    removed custom form and input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './views/home/home.component';
 import { MedewerkersbeheerComponent } from './views/medewerkersbeheer/medewerkersbeheer.component';
 import { AddMedewerkerComponent } from './views/add-medewerker/add-medewerker.component';
-import { FormComponent } from './components/form/form.component';
-import { InputComponent } from './components/input/input.component';
 import {TableModule} from 'primeng/table';
 import { EditMedewerkerComponent } from './views/edit-medewerker/edit-medewerker.component';
 import { ViewMedewerkerComponent } from './views/view-medewerker/view-medewerker.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ChangeMedewerkerStatusComponent } from './components/change-medewerker-status/change-medewerker-status.component';
-
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @NgModule({
   declarations: [
@@ -28,8 +26,6 @@ import { ChangeMedewerkerStatusComponent } from './components/change-medewerker-
     HomeComponent,
     MedewerkersbeheerComponent,
     AddMedewerkerComponent,
-    FormComponent,
-    InputComponent,
     EditMedewerkerComponent,
     ViewMedewerkerComponent,
     ChangeMedewerkerStatusComponent
@@ -39,7 +35,8 @@ import { ChangeMedewerkerStatusComponent } from './components/change-medewerker-
     AppRoutingModule,
     HttpClientModule,
     TableModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
